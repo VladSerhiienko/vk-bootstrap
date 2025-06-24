8beb55e74799feb2b1aa894f553341ce92746ed2
Vlad Serhiienko <vlad.serhiienko@gmail.com>
2025-06-24 13:33:15 -0700

    `conanfile.py`

---
+++ conanfile.py
@@ -0,0 +1,48 @@
+from conan import ConanFile
+from conan.tools.cmake import CMake, CMakeToolchain, CMakeDeps, cmake_layout
+
+class VkBootstrapConan(ConanFile):
+    name = "vk-bootstrap"
+    version = "1.4.313.0"
+    description = "Vulkan bootstraping library."
+    license = "MIT"
+    topics = ("vulkan", "bootstrap", "setup")
+    homepage = "https://github.com/charles-lunarg/vk-bootstrap"
+    url = "https://github.com/conan-io/conan-center-index"
+
+    settings = "os", "compiler", "build_type", "arch"
+    generators = "CMakeToolchain", "CMakeDeps", "AutotoolsToolchain", "PkgConfigDeps", "XcodeDeps"
+
+    options = {
+        "shared": [True, False],
+        "fPIC": [True, False],
+        "test": [True, False],
+        "werror": [True, False],
+    }
+    default_options = {
+        "shared": False,
+        "fPIC": True,
+        "test": False,
+        "werror": False,
+    }
+   
+    def build(self):
+        cmake = CMake(self)
+        cmake.configure()
+        cmake.build()
+
+    def layout(self):
+        cmake_layout(self)
+
+    def requirements(self):
+        self.requires(f"vulkan-headers/{self.version}")
+
+    def generate(self):
+        tc = CMakeToolchain(self)
+        tc.variables["VK_BOOTSTRAP_TEST"] = self.options.test
+        tc.variables["VK_BOOTSTRAP_WERROR"] = self.options.werror
+        tc.variables["VK_BOOTSTRAP_POSITION_INDEPENDENT_CODE"] = self.options.fPIC
+        tc.variables["BUILD_SHARED_LIBS"] = self.options.shared
+        tc.generate()
+        deps = CMakeDeps(self)
+        deps.generate()
